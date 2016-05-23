2be0d073b9e896c147625ecda1429580dc42b397
Venkatesh-Prasad Ranganath <rvprasad@ksu.edu>
2016-05-23 10:11:52 -0500

    Instead of wrapping and throwing IOException as a RuntimeException, initialize() now thorws IOException.

--- logging/src/main/java/dyco4jvm/instrumentation/logging/LoggerInitializer.java
+++ logging/src/main/java/dyco4jvm/instrumentation/logging/LoggerInitializer.java
@@ -7,16 +7,12 @@ import java.util.zip.GZIPOutputStream;
 public final class LoggerInitializer {
 
     @SuppressWarnings({"unused"})
-    public static void initialize() {
+    public static void initialize() throws IOException {
         final int _bufferLength = 10000000;
-        try {
-            final File _profileFile = File.createTempFile("trace_", ".gz", new File("."));
-            final OutputStream _stream = new FileOutputStream(_profileFile, true);
-            final PrintWriter _logWriter =
-                    new PrintWriter(new BufferedOutputStream(new GZIPOutputStream(_stream), _bufferLength));
-            Logger.initialize(_logWriter);
-        } catch (final IOException _e) {
-            throw new RuntimeException(_e);
-        }
+        final File _profileFile = File.createTempFile("trace_", ".gz", new File("."));
+        final OutputStream _stream = new FileOutputStream(_profileFile, true);
+        final PrintWriter _logWriter =
+                new PrintWriter(new BufferedOutputStream(new GZIPOutputStream(_stream), _bufferLength));
+        Logger.initialize(_logWriter);
     }
 }
