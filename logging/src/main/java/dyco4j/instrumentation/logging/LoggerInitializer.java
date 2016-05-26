c9c5a7784733b871ec6ce27b9be96844858ea5fb
Venkatesh-Prasad Ranganath <rvprasad@ksu.edu>
2016-05-26 02:20:08 -0500

    Bumped logging library to version 0.5.0
    Tweaked few Logger tests
    Added tests for LoggerInitializer
    Changed build.gradle to control jar creation
    Added support to generate coverage report

--- logging/src/main/java/dyco4j/instrumentation/logging/LoggerInitializer.java
+++ logging/src/main/java/dyco4j/instrumentation/logging/LoggerInitializer.java
@@ -1,18 +1,37 @@
 package dyco4j.instrumentation.logging;
 
 import java.io.*;
+import java.util.Properties;
 import java.util.zip.GZIPOutputStream;
 
 @SuppressWarnings("unused")
 public final class LoggerInitializer {
+    private static File traceFile;
 
-    @SuppressWarnings({"unused"})
     public static void initialize() throws IOException {
-        final File _traceFile = File.createTempFile("trace_", ".gz", new File("."));
-        final OutputStream _stream = new FileOutputStream(_traceFile, true);
-        final int _bufferLength = 10000000;
+        final Properties _properties = getProperties();
+        final File _folder = new File(_properties.getProperty("rootFolder", "."));
+        if (!_folder.exists())
+            _folder.mkdir();
+
+        LoggerInitializer.traceFile = File.createTempFile("trace_", ".gz", _folder);
+        final OutputStream _stream = new FileOutputStream(LoggerInitializer.traceFile, true);
+        final int _bufferLength = Integer.parseInt(_properties.getProperty("bufferLength", "10000000"));
         final PrintWriter _logWriter =
                 new PrintWriter(new BufferedOutputStream(new GZIPOutputStream(_stream), _bufferLength));
         Logger.initialize(_logWriter);
+    }
+
+    static File getTraceFile() {
+        return traceFile;
+    }
+
+    private static Properties getProperties() throws IOException {
+        final Properties _tmp = new Properties();
+        try (final InputStream _in1 = LoggerInitializer.class.getResourceAsStream("logging.properties")) {
+            if (_in1 != null)
+                _tmp.load(_in1);
+        }
+        return _tmp;
     }
 }
---
+++ logging/src/test/java/dyco4j/instrumentation/logging/LoggerInitializerTest.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2016, Venkatesh-Prasad Ranganath
+ *
+ * BSD 3-clause License
+ *
+ * Author: Venkatesh-Prasad Ranganath (rvprasad)
+ *
+ */
+
+package dyco4j.instrumentation.logging;
+
+import org.junit.Test;
+
+import java.io.*;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.text.MessageFormat;
+import java.util.Properties;
+import java.util.zip.GZIPInputStream;
+
+public class LoggerInitializerTest {
+    @Test
+    public void testInitializeWithPropertiesFile() throws Exception {
+        LoggerInitializer.initialize();
+        final String _msg = "test initialize with properties file";
+        Logger.log(_msg);
+        Logger.cleanupForTest();
+
+        final Properties _tmp = new Properties();
+        try (final InputStream _in1 = LoggerInitializer.class.getResourceAsStream("logging.properties")) {
+            if (_in1 != null)
+                _tmp.load(_in1);
+        }
+        assert _tmp.getProperty("rootFolder") != null : "rootFolder property not found";
+
+        checkTraceFilesForLogs(_msg);
+    }
+
+    @Test
+    public void testInitializeWithoutPropertiesFile() throws Exception {
+        // TODO: change the name of build/resources/test/dyco4j/instrumentation/logging/logging.properties
+        final Path _srcPropFilePath =
+                Paths.get("build", "resources", "test", "dyco4j", "instrumentation", "logging", "logging.properties");
+        final File _srcPropFile = _srcPropFilePath.toFile();
+        final Path _destPropFilePath = _srcPropFilePath.resolveSibling(Paths.get("logging.properties1"));
+        final File _destPropFile = _destPropFilePath.toFile();
+        assert _srcPropFile.renameTo(_destPropFile) : "property file could not be renamed";
+
+        LoggerInitializer.initialize();
+        final String _msg = "test initialize without properties file";
+        Logger.log(_msg);
+        Logger.cleanupForTest();
+
+        assert _destPropFile.renameTo(_srcPropFile) : "property file renaming could not be undone.";
+
+        checkTraceFilesForLogs(_msg);
+    }
+
+    private void checkTraceFilesForLogs(final String expectedMessage) throws IOException {
+        final File _traceFile = LoggerInitializer.getTraceFile();
+        try (final BufferedReader _traceReader =
+                new BufferedReader(new InputStreamReader(new GZIPInputStream(new FileInputStream(_traceFile))))) {
+            _traceReader.readLine();
+            final String _line = _traceReader.readLine();
+            final String _regex = MessageFormat.format("^1,\\d+,{0}$", expectedMessage);
+            assert _line.matches(_regex) : "expected log statement not found";
+        }
+        assert _traceFile.delete() : "Could not delete trace file";
+    }
+}
